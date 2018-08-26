75f3e57c1d68cbe4abc0e40484076a1704a0c52c
“liuhaipeng” <>
2018-08-26 17:24:48 +0800

    增加一个完整的spring boot测试框架

---
+++ com.diaosichengxuyuan.spring.parent/com.diaosichengxuyuan.spring.boot/src/main/java/com/diaosichengxuyuan/spring/boot/common/ValidationUtil.java
@@ -0,0 +1,32 @@
+package com.diaosichengxuyuan.spring.boot.common;
+
+import org.apache.commons.collections4.CollectionUtils;
+
+import javax.validation.*;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * 校验器
+ *
+ * @author liuhaipeng
+ * @date 2018/8/24
+ */
+public class ValidationUtil {
+
+    /**
+     * 校验对象参数是否合法
+     *
+     * @param object
+     * @throws IllegalArgumentException
+     */
+    public static void validate(Object object) {
+        Validator validator = Validation.buildDefaultValidatorFactory().usingContext().getValidator();
+        Set<ConstraintViolation<Object>> constraintViolations = validator.validate(object);
+        if(CollectionUtils.isNotEmpty(constraintViolations)) {
+            String message = constraintViolations.stream().map(c -> c.getPropertyPath() + " " + c.getMessage()).collect(
+                Collectors.toList()).toString();
+            throw new IllegalArgumentException(message);
+        }
+    }
+}
---
+++ com.diaosichengxuyuan.spring.parent/com.diaosichengxuyuan.spring.boot/src/main/java/com/diaosichengxuyuan/spring/boot/service/test/TestService.java
@@ -0,0 +1,25 @@
+package com.diaosichengxuyuan.spring.boot.service.test;
+
+import com.diaosichengxuyuan.spring.boot.common.Page;
+import com.diaosichengxuyuan.spring.boot.service.test.dto.TestDTO;
+
+import java.util.List;
+
+/**
+ * web层
+ *
+ * @author liuhaipeng
+ * @date 2018/8/24
+ */
+public interface TestService {
+
+    TestDTO selectById(Long id);
+
+    List<TestDTO> selectAll();
+
+    List<TestDTO> selectPage(Page page);
+
+    int insert(TestDTO testDTO);
+
+    int deleteById(Long id);
+}
