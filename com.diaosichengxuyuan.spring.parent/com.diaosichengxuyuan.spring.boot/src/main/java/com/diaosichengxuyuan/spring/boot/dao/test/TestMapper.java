8584e40b648b68f36e363e43c91c53f1cfefb514
“liuhaipeng” <>
2018-12-30 11:57:19 +0800

    删除不要的工程

--- com.diaosichengxuyuan.spring.parent/com.diaosichengxuyuan.spring.boot/src/main/java/com/diaosichengxuyuan/spring/boot/dao/test/TestMapper.java
+++
@@ -1,7 +0,0 @@
-package com.diaosichengxuyuan.spring.boot.dao.test;
-
-import com.diaosichengxuyuan.spring.boot.dao.test.entity.TestDO;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface TestMapper extends Mapper<TestDO> {
-}
--- com.diaosichengxuyuan.spring.parent/com.diaosichengxuyuan.spring.mvc/src/main/java/com/diaosichengxuyuan/spring/mvc/dao/test/TestMapper.java
+++
@@ -1,7 +0,0 @@
-package com.diaosichengxuyuan.spring.mvc.dao.test;
-
-import com.diaosichengxuyuan.spring.mvc.dao.test.entity.TestDO;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface TestMapper extends Mapper<TestDO> {
-}
